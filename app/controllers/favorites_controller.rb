e7115dcfafa34bf96c58f646ed64dc3a0a92db3b
vagrant <vagrant@localhost.localdomain>
2020-07-03 16:53:30 +0000

    フラッシュメッセージ追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
   def create
     @stretch = Stretch.find(params[:stretch_id])
     @favorite = current_user.favorites.create!(stretch_id: params[:stretch_id])
-    flash[:success] = 'この投稿をお気に入りに登録しました'
     @favorite.save
   end
 
   def destroy
     @stretch = Stretch.find(params[:stretch_id])
     @favorite = Favorite.find(params[:id])
-    flash[:danger] = 'この投稿のお気に入りを解除しました'
     if @favorite.destroy
       @favorite = Favorite.find_by(params[:id])
     end
