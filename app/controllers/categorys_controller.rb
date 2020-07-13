b42090adba6d0ca8a943f82013d1f6b5cd5a6d05
vagrant <vagrant@localhost.localdomain>
2020-07-13 04:16:51 +0000

    ヘッダー修正

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -7,7 +7,7 @@ class CategorysController < ApplicationController
     @categorys = Category.where(is_valid: true)
     @category = Category.find(params[:id])
     @q = @category.stretchs.all.ransack(params[:q])
-    @stretchs = @q.result(distinct: true).page(params[:page]).per(8)
+    @stretchs = @q.result(distinct: true).page(params[:page])
     @title = @category.name
     render 'stretchs/index'
   end
