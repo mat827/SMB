f5c971c203aee888f7208a6ebdf293adad72b558
vagrant <vagrant@localhost.localdomain>
2020-08-12 12:28:48 +0000

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,8 +101,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            'ENV['MAIL_NAME']',
-    password:             'ENV['MAIL_PASSWORD']',
+    user_name:            ENV['MAIL_NAME'],
+    password:             ENV['MAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
